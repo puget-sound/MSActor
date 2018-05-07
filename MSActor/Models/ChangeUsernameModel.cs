0976249612ccb599cf0da4364a1d0a5c252c7aac
athabasca service account <aaron@goff.io>
2018-05-07 16:12:21 -0700

    Missing models

---
+++ MSActor/Models/ChangeUsernameModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSActor.Models
+{
+    public class ChangeUsernameModel
+    {
+        public string employeeid;
+        public string searchbase;
+        public string samaccountname;
+        public string userprincipalname;
+
+        public ChangeUsernameModel(string employeeid, string searchbase, string samaccountname, string userprincipalname)
+        {
+            this.employeeid = employeeid;
+            this.searchbase = searchbase;
+            this.samaccountname = samaccountname;
+            this.userprincipalname = userprincipalname;
+        }
+    }
+}
---
+++ MSActor/Models/SetPasswordModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSActor.Models
+{
+    public class SetPasswordModel
+    {
+        public string employeeid;
+        public string samaccountname;
+        public string accountpassword;
+        public string changepasswordatlogon;
+
+        public SetPasswordModel(string employeeid, string samaccountname, string accountpassword, string changepasswordatlogon)
+        {
+            this.employeeid = employeeid;
+            this.samaccountname = samaccountname;
+            this.accountpassword = accountpassword;
+            this.changepasswordatlogon = changepasswordatlogon;
+        }
+    }
+}
